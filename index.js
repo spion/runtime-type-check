a68a5048cef46c8e145fee2faf2dcf2e2d12ea97
Gorgi Kosev <gorgi.kosev@gmail.com>
2014-11-19 15:51:45 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+var TC = {};
+
+TC.type = function(typestr) {
+    return function(msg) {
+        return function(p) {
+            if (typeof(p) !== typestr)
+                return "must be a " + typestr;
+        }
+    }
+}
+TC.string = TC.type('string');
+TC.number = TC.type('number');
+TC.boolean = TC.type('boolean');
+
+TC.object = function (spec) {
+    return function(obj) {
+        if (obj == null)
+            return "must be a valid object";
+        for (var key in spec)
+            if (spec.hasOwnProperty(key)) {
+                var test = spec[key](obj[key], key);
+                if (test != null) 
+                    return makeMessage('property', key, test);
+            }
+    }
+}
+
+TC.array = function (itemChecker) {
+    return function(arr) {
+        if (Object.prototype.toString.call(arr) !== '[object Array]')
+            return "must be an array";
+        for (var k = 0; k < arr.length; ++k) {
+            var result = itemChecker(arr[k]);
+            if (result != null) return makeMessage('index', k, result);
+        }
+    }
+}
+
+TC.optional = function(checker) {
+    return function(item) {
+        if (item != null) {
+            var result = checker(item);
+            if (result) return result + ' or null';
+        }
+    }
+}
+
+TC.either = function(checker1, checker2) {
+    return function(item) {
+        var r1 = checker1(item), r2 = checker2(item);
+        if (r1 != null && r2 != null)
+            return "either " + r1 + " or " + r2;
+    }
+}
+
+TC.both = function(checker1, checker2) {
+    return function(item) {
+        var r1 = checker1(item), r2 = checker2(item);
+        return r1 && r2;
+    }
+}
+
+// Helper function for messages that contain keys
+function makeMessage(kind, key, msg) {
+    return kind + ' ' + key + " " + msg;
+}
+
+TC.check = function(type, val, msg) {
+    msg = msg ? msg + ': ' : ''
+    var m = type(val);
+    if (m != null) throw new TypeError(msg + m);
+}
+
+module.exports = TC;
+
+
---
+++ test/index.js
@@ -0,0 +1,23 @@
+var t = require('tape');
+
+
+var TC = require('../');
+
+// A simple test
+var Person = TC.object({
+    name: TC.string(),
+    age: TC.number(),
+    hobbies: TC.array(TC.string()),
+    partner: TC.optional(TC.string())
+});
+
+function logIsPerson(obj) {
+    try { TC.check(Person, obj, 'obj'); }
+    catch (e) { console.log(e.message); }
+}
+
+
+logIsPerson({name: "John", age: 128});
+logIsPerson({name: "John", age: 256, hobbies: [1,2,3]});
+logIsPerson({name: 123, age: 512});
+logIsPerson({name: "John", age: 1024, partner: false, hobbies: []});
